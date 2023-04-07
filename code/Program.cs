9849870ca0c58599d82d6102a552f56d0cd3381a
Vlad <vlad.khvesyukovich@gmail.com>
2023-04-07 19:44:05 +0300

    t17 is not done yeet

--- code/Program.cs
+++ code/Program.cs
@@ -15,7 +15,6 @@ namespace code
 
             bool[] checkNum = new bool[s.Length]; // positions of nums
             string noNums = ""; // string with no nums
-
             for (int i = 0; i < s.Length; i++)
             {
                 if (s[i] != '0' && s[i] != '1' && s[i] != '2' && s[i] != '3' && s[i] != '4' && s[i] != '5' && s[i] != '6' && s[i] != '7' && s[i] != '8' && s[i] != '9')
@@ -32,11 +31,67 @@ namespace code
             }
             string[] combinations = new string[(int)amount + 1];
 
-            // create
-            int position = 0;
+            // create combinations
             for (int i = 0; i < combinations.Length; i++)
             {
+                string num = ""; // binary number 0101.... for combinations
+                int index = 0;
+                int currentNum = i;
+                int[] a = new int[noNums.Length];
+                for (int i2 = 0; currentNum > 0;  i2++) 
+                {
+                    a[i2] = currentNum % 2;
+                    currentNum /= 2;
+                    index = i2;
+                }
+                for (int i2 = index; i2 >= 0; i2--)
+                {
+                    num += a[i2];
+                }
+                while (num.Length < noNums.Length) 
+                {
+                    num = "0" + num;
+                }
+                combinations[i] = num;
+            }
 
+            // check combinations for all numbers (upper/lower case)
+            Console.WriteLine();
+            for (int i = 0; i < combinations.Length; i++)
+            {
+                Console.WriteLine($"combination[{i}] = " + combinations[i]);
+            }
+            Console.WriteLine();
+
+            // output
+            Console.WriteLine("\nOutput:");
+            string[] outputs = new string[combinations.Length];
+            for (int i = 0; i < combinations.Length; i++)
+            {
+                int index = 0;
+                for (int i2 = 0; i2 < s.Length; i2++)
+                {
+                    if (checkNum[i2] == true) // check if number
+                    {
+                        outputs[i] += s[i2];
+                    }
+                    else
+                    {
+                        if (combinations[i][index] == '0')
+                        {
+                            Console.WriteLine("1)");
+                            outputs[i] += Convert.ToString(s[i2]).ToLower();
+                            index++;
+                        }
+                        else if (combinations[i][index] == '1')
+                        {
+                            Console.WriteLine("2)");
+                            outputs[i] += Convert.ToString(s[i2]).ToUpper();
+                            index++;
+                        }
+                    }
+                }
+                Console.WriteLine(outputs[i]);
             }
 
 
